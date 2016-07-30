b02cf44471186a74f64e1c139c8ca118e826b78f
Ryan O'Rourke <rydevils@comcast.net>
2016-07-30 00:14:24 -0400

    Initial Commit

---
+++ CurrentLocationTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CurrentLocationTracker
+//
+//  Created by Ryan O'Rourke on 7/30/16.
+//  Copyright © 2016 Ryan O'Rourke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CurrentLocationTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CurrentLocationTracker
+//
+//  Created by Ryan O'Rourke on 7/30/16.
+//  Copyright © 2016 Ryan O'Rourke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
