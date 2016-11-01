883a489f1e33f066468574cdb1f3c9f96a04c7db
GRADY <gjenkins205@gmail.com>
2016-11-01 13:42:08 -0400

    first commit

---
+++ CurrentLocationTracker/DestinationMapPin.swift
@@ -0,0 +1,9 @@
+//
+//  DestinationMapPin.swift
+//  CurrentLocationTracker
+//
+//  Created by Ryan O'Rourke on 7/31/16.
+//  Copyright © 2016 Ryan O'Rourke. All rights reserved.
+//
+
+import Foundation
---
+++ CurrentLocationTracker/FriendsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsListTableViewCell.swift
+//  CurrentLocationTracker
+//
+//  Created by Ryan O'Rourke on 7/31/16.
+//  Copyright © 2016 Ryan O'Rourke. All rights reserved.
+//
+
+import UIKit
+
+class FriendsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
